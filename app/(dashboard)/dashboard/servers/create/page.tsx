d4d4613bc479e2abf18037d9345716c64505c0b1
Masato Kikuchi <m-kikuchi@coe.ad.jp>
2025-06-10 12:55:53 +0900

    Implement code changes to enhance functionality and improve performance

--- app/(dashboard)/dashboard/servers/create/page.tsx
+++ app/(dashboard)/dashboard/servers/create/page.tsx
@@ -1,6 +1,24 @@
+"use client";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { NextPage } from "next";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+    name: z.string().min(1, "Server name is required"),
+    type: z.string().min(1, "Server type is required"),
+    os: z.string().min(1, "Operating system is required"),
+})
 
 const Page: NextPage = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+    })
+    const handleSubmit = async (values: z.infer<typeof formSchema>) => {
+
+    }
     return (
         <>
             <div>
@@ -8,6 +26,28 @@ const Page: NextPage = () => {
                     Add Server
                 </h1>
             </div>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8 mt-6">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Server name</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="Enter server name"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormDescription>
+                                    This is the name of the server that will be displayed in the dashboard.
+                                </FormDescription>
+                            </FormItem>
+                        )}
+                    />
+                </form>
+            </Form>
         </>
     );
 };
