47285e2eda8cd57eb63f04301eb41470ebcca7e4
tuna2134 <hello@tuna2134.dev>
2025-07-08 22:59:32 +0900

    feat: implement server creation with dynamic password generation and network configuration

--- app/(dashboard)/dashboard/servers/create/action.ts
+++ app/(dashboard)/dashboard/servers/create/action.ts
@@ -4,16 +4,55 @@ import { auth } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { server } from "@/lib/db/schemas";
 import { headers } from "next/headers";
+import { plans } from "@/data/config.json";
+
+function generatePassword(length: number): string {
+    const charset =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
 
 export async function createServer(name: string, type: string, os: string) {
     const session = await auth.api.getSession({
         headers: await headers(),
     });
-    const id = crypto.randomUUID();
+    const address = "192.168.122.3/24";
+    const gateway = "192.168.122.1";
+    const plan = plans.find((plan) => plan.id === parseInt(type));
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_VM_CONTROLLER_ENDPOINT}/domains`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                password: generatePassword(16),
+                network: {
+                    address: address,
+                    gateway: gateway,
+                },
+                resources: {
+                    cpu: plan?.resource.cpu,
+                    memory: plan?.resource.memory,
+                    disk: `${plan?.resource.disk}G`,
+                },
+            }),
+        },
+    );
+    const data = await res.json();
+    if (!res.ok) {
+        throw new Error(data.message || "Failed to create server");
+    }
     const serverData: typeof server.$inferInsert = {
-        id,
+        id: data.id,
         name,
-        ip: "192.168.1.0/24",
+        ip: address,
         os,
         type: parseInt(type),
         authorId: session?.user.id as string,
