386a7e4a8c8a564c2d4b90d4193c38d31b903bcd
tuna2134 <hello@tuna2134.dev>
2025-07-11 06:52:50 +0900

    feat: update Page component to display VPS information and structure

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,7 +1,30 @@
 import { NextPage } from "next";
 
 const Page: NextPage = () => {
-    return <p>aaaa</p>;
+    return (
+        <>
+            <div className="flex h-[90vh] items-center">
+                <div>
+                    <h1 className="text-3xl font-bold tracking-wider text-purple-500">
+                        学生にVPSを...
+                    </h1>
+                    <p className="mt-4 text-gray-700">
+                        たいていのVPSは値段が高い...
+                        <br />
+                        学生からすると、財布が痛い...
+                        <br />
+                        そんな学生のために、StuVPSは学生向けのVPSを提供します。
+                    </p>
+                </div>
+            </div>
+            <div className="mt-6">
+                <h2 className="text-xl font-semibold tracking-wider text-purple-500">
+                    運用体制
+                </h2>
+                <p>ベストエフォート型となっています。</p>
+            </div>
+        </>
+    );
 };
 
 export default Page;
