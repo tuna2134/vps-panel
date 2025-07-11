d2e6afd0ee60c4902b3a160295d27730f4065aa6
tuna2134 <hello@tuna2134.dev>
2025-07-12 06:50:18 +0900

    feat: add setup script creation page with title

--- app/(dashboard)/dashboard/setup-scripts/create/page.tsx
+++ app/(dashboard)/dashboard/setup-scripts/create/page.tsx
@@ -1,7 +1,14 @@
+import { Form } from "@/components/ui/form";
 import { NextPage } from "next";
 
 const Page: NextPage = () => {
-    return <></>
+    return (
+        <>
+            <div>
+                <h2 className="text-2xl font-bold tracking-wider">セットアップスクリプトの作成</h2>
+            </div>
+        </>
+    );
 }
 
 export default Page;
