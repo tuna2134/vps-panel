df8853632e9c5b0fd4c7a7980a1aa259eb94d1ee
tuna2134 <hello@tuna2134.dev>
2025-07-11 12:55:48 +0900

    feat: enhance Header component with user session management and avatar display

--- components/pages/main/Header.tsx
+++ components/pages/main/Header.tsx
@@ -1,7 +1,12 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useSession } from "@/lib/auth-client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Header: React.FC = () => {
+    const session = useSession();
+    const isLoggedIn = !!session.data;
     return (
         <header className="flex h-16 w-full items-center border-b">
             <div className="mx-auto flex w-full max-w-3xl items-center justify-between px-4">
@@ -11,9 +16,18 @@ const Header: React.FC = () => {
                     <Link href="/supporter">Supporter</Link>
                 </div>
                 <div>
-                    <Button asChild>
-                        <Link href="/dashboard">Login</Link>
-                    </Button>
+                    {isLoggedIn ? (
+                        <Link href="/dashboard" className="flex items-center">
+                            <Avatar>
+                                <AvatarImage src={session.data?.user?.image || ""} alt="User Avatar" />
+                                <AvatarFallback>{session.data?.user?.name}</AvatarFallback>
+                            </Avatar>
+                        </Link>
+                    ) : (
+                        <Button asChild>
+                            <Link href="/dashboard">Login</Link>
+                        </Button>
+                    )}
                 </div>
             </div>
         </header>
